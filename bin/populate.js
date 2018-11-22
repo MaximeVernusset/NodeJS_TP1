d75157a1aa27680e63946b7bd99e9b738563c610
Maxime Vernusset <maxime.vernusset@edu.ece.fr>
2018-11-22 14:13:04 +0100

    Métriques pour key=1 et 2

--- bin/populate.js
+++ bin/populate.js
@@ -2,13 +2,13 @@
 "use strict";
 exports.__esModule = true;
 var metrics_1 = require("../src/metrics");
-var met = [
+var met1 = [
     new metrics_1.Metric("" + new Date('2013-11-04 14:00 UTC').getTime(), 12),
     new metrics_1.Metric("" + new Date('2013-11-04 14:15 UTC').getTime(), 10),
     new metrics_1.Metric("" + new Date('2013-11-04 14:30 UTC').getTime(), 8)
 ];
 var db = new metrics_1.MetricsHandler('./db');
-db.save("2", met, function (err) {
+db.save("1", met1, function (err) {
     if (err)
         throw err;
     console.log('Data populated');
--- bin/populate.ts
+++ bin/populate.ts
@@ -2,7 +2,12 @@
 
 import { Metric, MetricsHandler } from '../src/metrics';
 
-const met = [
+const met1 = [
+  new Metric(`${new Date('2013-11-04 14:00 UTC').getTime()}`, 12),
+  new Metric(`${new Date('2013-11-04 14:15 UTC').getTime()}`, 10),
+  new Metric(`${new Date('2013-11-04 14:30 UTC').getTime()}`, 8)
+];
+const met2 = [
   new Metric(`${new Date('2013-11-04 14:00 UTC').getTime()}`, 12),
   new Metric(`${new Date('2013-11-04 14:15 UTC').getTime()}`, 10),
   new Metric(`${new Date('2013-11-04 14:30 UTC').getTime()}`, 8)
@@ -10,7 +15,11 @@ const met = [
 
 const db = new MetricsHandler('./db');
 
-db.save("2", met, (err: Error | null) => {
+db.save("1", met1, (err: Error | null) => {
+  if (err) throw err;
+  console.log('Data populated');
+});
+db.save("2", met2, (err: Error | null) => {
   if (err) throw err;
   console.log('Data populated');
 });
