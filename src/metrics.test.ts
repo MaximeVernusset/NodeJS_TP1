527d967b96d35b765e77ec2ffbcb8b3b3dce5a8f
Maxime Vernusset <maxime.vernusset@edu.ece.fr>
2018-11-26 09:48:56 +0100

    Tests sur metrics (Todo:  save & delete)

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -1,13 +1,46 @@
 import { expect } from 'chai';
+import { Metric, MetricsHandler } from './metrics';
+import { LevelDb } from "./leveldb";
 
-const a: number = 0;
+const dbPath: string = 'db_test';
+var dbMet: MetricsHandler;
 
 describe('Metrics', function () {
-    describe('#get', function() {
-        it('should equal 0', function () {
-            expect(a).to.equal(0);
-         });
-    });
-})
+  before(function () {
+    LevelDb.clear(dbPath);
+    dbMet = new MetricsHandler(dbPath);
+  });
 
-  
+  after(function () {
+    dbMet.db.close();
+  });
+
+  describe('#get', function () {
+    it('should get empty array on non existing group', function () {
+      dbMet.get("0", function (err: Error | null, result?: Metric[]) {
+        expect(err).to.be.null;
+        expect(result).to.not.be.undefined;
+        expect(result).to.be.empty;
+        expect(result).to.be.an('array');
+      });
+    });
+  });
+
+  describe('#save', function () {
+    it('should save data', function () {
+      //TODO
+    });
+    it('should update data', function () {
+      //TODO
+    });
+  });
+
+  describe('#delete', function () {
+    it('should delete data', function () {
+      //TODO
+    });
+    it('should not fail if data does not exist', function () {
+      //TODO
+    });
+  });
+});
